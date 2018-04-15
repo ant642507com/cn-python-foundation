626e9709f583a6626b80727ea640550d5ac3bbe7
Wang Sheng <ant642507com@163.com>
2018-04-15 08:14:27 +0800

    first commit

---
+++ data-analyse-base/tmdb-movies/test.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 14 14:50:25 2018
+
+@author: admin
+"""
+
+import pandas as pd
+
+movie_df = pd.read_csv('tmdb-movies.csv')
+
+tmp_df = movie_df.head(10)
+
+movie_df.dropna(axis=0, inplace=True, subset=list(['genres']))
+movie_df.dropna(axis=0, inplace=True, subset=list(['director']))
+
+def deal_columns_to_rows(movie_df, columnName):
+    movie_type_df = movie_df[columnName].str.split('|', expand=True)
+
+    movie_type_df['id'] = movie_df['id']
+    
+    movie_df_merged = movie_df.merge(movie_type_df)
+    
+    movie_df_merged.drop_duplicates(subset=None, keep='first', inplace=True)
+    
+    movie_column = movie_df.columns.values.tolist()
+    
+    movie_column.remove(columnName)
+    
+    melted = pd.melt(movie_df_merged, id_vars=movie_column, value_vars=[0, 1, 2, 3, 4],
+                     value_name=columnName+'_uniq').drop('variable', axis = 1).dropna()
+    
+    return melted
+
+melted = deal_columns_to_rows(movie_df, 'genres')
+
+dealed_df = melted.copy()
+
+"""
+根据电影种类，统计出最受欢迎的电影类型
+"""
+mean_dealed_df = dealed_df.groupby('genres_uniq')['popularity'].mean()
+
+mean_dealed_df.plot(kind='bar')
+
+
+"""
+在最受欢迎的种类的电影中，都是由哪些年出的作品，数量占比是怎样的
+"""
+popular_df = melted.query('genres_uniq==\'Adventure\'')
+#melted = deal_columns_to_rows(popular_df, 'release_year')
+
+dealed_cast_df = melted.copy()
+
+count_of_cast = dealed_cast_df.groupby('release_year')['release_year'].count()
+count_of_cast.plot(kind='bar' ,figsize=(15, 15))
+
+
+
